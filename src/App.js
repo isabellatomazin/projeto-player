4748b9b4aa8d59af8bbf014f86a6e0158a530ec1
Isabella Damiani Tomazin <isabella@pop-os.localdomain>
2022-07-07 08:31:32 -0300

    primeiro commit login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="container">
+    <div className="container-login">
+      <div className="wrap-login">
+        <form className="login-form">
+          <spam className="login-form-title">Bem vindo!</spam>
+         <div className="wrap-input">
+          <input className='input' type="Username"/>
+          <span className="focus-input" data-placeholder="Username"></span>
+         </div>
+
+         <div className="wrap-input">
+          <input className='input' type="Password"/>
+          <span className="focus-input" data-placeholder="Password"></span>
+         </div>
+
+          <div className="container-login-form-btn">
+            <button className="login-form-btn">Login</button>
+
+            <div className="text-center">
+              <span className="text1">Não possui conta?  </span>
+              <a className="text2" href="#"> Sign in</a>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
+   </div>
   );
 }
-
 export default App;
